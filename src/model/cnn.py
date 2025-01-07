f785a1b12ef7541d2961be41cc4d294e825f3983
mizaqq <michal.zareba@softwaremill.pl>
2025-01-07 23:40:20 +0100

    feature: added dynamic initiation of forward pass

--- src/model/cnn.py
+++ src/model/cnn.py
@@ -1,23 +1,28 @@
+import hydra
 import torch
 import torch.nn.functional as F
-import hydra
+
 
 class Convolutional(torch.nn.Module):
     def __init__(self, cfg) -> None:
         super(Convolutional, self).__init__()
-        layers = {k:hydra.utils.instantiate(v) for k,v in cfg.models.params.layers.items()}
-        for key in layers: 
-            setattr(self, key, layers[key])
-             
+        self.layers = torch.nn.ModuleDict({k: hydra.utils.instantiate(v) for k, v in cfg.models.params.layers.items()})
+        self.pool_indices = cfg.models.params.get("pool_indices", [])
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = self.pool3(F.relu(self.conv3(x)))
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        x = F.softmax(x, dim=1)
-        return x
+        for idx, (_, layer) in enumerate(self.layers.items()):
+            if isinstance(layer, torch.nn.Conv2d):
+                x = F.relu(layer(x))
+                if idx in self.pool_indices:
+                    x = F.max_pool2d(x, 2)
+            elif isinstance(layer, torch.nn.Linear):
+                if x.dim() > 2:
+                    x = x.view(-1, self.num_flat_features(x))
+                x = F.relu(layer(x))
+            elif isinstance(layer, torch.nn.Dropout):
+                x = layer(x)
+        return F.softmax(x, dim=1)
+
     @staticmethod
     def num_flat_features(x: torch.Tensor) -> int:
         size = x.size()[1:]
