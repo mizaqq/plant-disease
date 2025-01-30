65d95cf148bf38cc8c7adcb3e21167159b5f8720
mizaqq <michal.zareba@softwaremill.pl>
2025-01-30 18:29:54 +0100

    feature: added new params files, ran experiments

--- src/utils/mlflow.py
+++ src/utils/mlflow.py
@@ -1,17 +1,24 @@
 import os
+from typing import Optional
 
 import mlflow
 
 
 class MLFlowRunManager:
-    def __init__(self) -> None:
-        self.manager = mlflow
-        self.login()
-        self.manager.start_run()
-
-    def login(
-        self, uri: str = os.environ.get("MLFLOW_URI", "http://localhost:5000"), experiment: str = "default"
+    def __init__(
+        self,
+        run_id: Optional[str] = None,
+        uri: str = os.environ.get("MLFLOW_URI", "http://localhost:5000"),
+        experiment: str = "default",
     ) -> None:
+        self.manager = mlflow
+        self.login(uri, experiment)
+        if run_id:
+            self.manager.start_run(run_id)
+        else:
+            self.manager.start_run()
+
+    def login(self, uri: str, experiment: str) -> None:
         self.manager.set_tracking_uri(uri)
         self.manager.set_experiment(experiment)
 
